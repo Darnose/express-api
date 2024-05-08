83fcd805d74284c46de9d701a21efd34803cd923
Andrey <andrey.yarovoy.2013@gmail.com>
2024-05-08 11:08:29 +0300

    created function methods

---
+++ controllers/user-controller.js
@@ -0,0 +1,9 @@
+const UserController = {
+  register: async () => {},
+  login: async () => {},
+  getUserbyId: async () => {},
+  updateUser: async () => {},
+  current: async () => {},
+};
+
+module.exports = UserController;
