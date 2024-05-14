dfb09e7d9b32048f117744e4f2d82cfc9aa2df2b
Darnose <andrey.yarovoy.2013@gmail.com>
2024-05-14 10:22:25 +0300

    createPost route

---
+++ controllers/post-controller.js
@@ -0,0 +1,38 @@
+const { prisma } = require('../prisma/prisma-client');
+
+const PostController = {
+  createPost: async (req, res) => {
+    const { content } = req.body;
+
+    const authorId = req.user.userId;
+
+    if (!content) {
+      return res.status(400).json({ error: 'Bce поля обязательны!' });
+    }
+
+    try {
+      const post = await prisma.post.create({
+        data: {
+          content,
+          authorId,
+        },
+      });
+
+      res.json(post);
+    } catch (error) {
+      console.error('Error in createPost', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
+  getAllPost: async (req, res) => {
+    res.send('getAllPost');
+  },
+  getPostById: async (req, res) => {
+    res.send('getPostById');
+  },
+  deletePost: async (req, res) => {
+    res.send('deletePost');
+  },
+};
+
+module.exports = PostController;
