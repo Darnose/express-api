002d3e9b4ad4c0aa956c67f7954cf7d404a7fa55
Darnose <andrey.yarovoy.2013@gmail.com>
2024-05-18 11:10:17 +0300

    created likePost

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { UserController, PostController, CommentController } = require('../controllers');
+const {
+  UserController,
+  PostController,
+  CommentController,
+  LikeController,
+} = require('../controllers');
 const authenticateToken = require('../middleware/auth');
 
 const uploadsDestination = 'uploads';
@@ -31,5 +36,9 @@ router.delete('/posts/:id', authenticateToken, PostController.deletePost);
 // Comment routes
 router.post('/comments', authenticateToken, CommentController.createComment);
 router.delete('/comments/:id', authenticateToken, CommentController.deleteComment);
+
+// Like routes
+router.post('/likes', authenticateToken, LikeController.likePost);
+router.delete('/likes/:id', authenticateToken, LikeController.unLikePost);
 
 module.exports = router;
