f3975949bccfdf770a2c4de6ef4a7e9459342086
Darnose <andrey.yarovoy.2013@gmail.com>
2024-05-19 21:53:34 +0300

    created unLikePost

--- controllers/like-controller.js
+++ controllers/like-controller.js
@@ -29,7 +29,31 @@ const LikeController = {
     }
   },
   unLikePost: async (req, res) => {
-    res.send('unLikePost');
+    const { id } = req.params;
+    const userId = req.user.userId;
+
+    if (!id) {
+      return res.status(400).json({ error: 'Вы уже поставили дизлайк' });
+    }
+
+    try {
+      const existingLike = await prisma.like.findFirst({
+        where: { postId: id, userId },
+      });
+
+      if (!existingLike) {
+        return res.status(400).json({ error: 'Нельзя поставить дизлайк' });
+      }
+
+      const like = await prisma.like.deleteMany({
+        where: { postId: id, userId },
+      });
+
+      res.json(like);
+    } catch (error) {
+      console.error('unLikePost error', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
   },
 };
 
